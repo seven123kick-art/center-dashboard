8d39fab3f125bccb9ef8c0aec0722d950e440a21
seven123kick-art <seven123kick@gmail.com>
2026-05-03 23:25:21 +0900

    Update field_capacity.js

--- field_capacity.js
+++ field_capacity.js
@@ -1,9 +1,5 @@
-/* field_capacity.js : disabled after integration into app.js
-   2026-05-03
-   キャパ分析は app.js の CAPACITY_UI に統合済み。
-   二重描画とメモリ負荷を避けるため、このファイルは何もしません。
-*/
+/* field_capacity.js : disabled - capacity is integrated in app.js */
 'use strict';
 (function(){
-  window.FIELD_CAPACITY_UI = window.FIELD_CAPACITY_UI || { render(){ if (window.CAPACITY_UI) CAPACITY_UI.render(); } };
+  window.FIELD_CAPACITY_UI = window.FIELD_CAPACITY_UI || { render(){ /* disabled */ } };
 })();
